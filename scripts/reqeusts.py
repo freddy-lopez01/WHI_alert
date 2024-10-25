095463bef7563cf3afb4fac1cbd91b6d2ae46294
Freddy Lopez <freddlopez01@gmail.com>
2024-10-25 15:57:35 -0700

    pushing requests.py

--- scripts/reqeusts.py
+++ scripts/reqeusts.py
@@ -1,17 +1,28 @@
 import requests
+import json
 
 
 def fetch_api_data(api_url):
     try:
         response = requests.get(api_url)
         response.raise_for_status()  # Check if the request was successful
-        # Print the content type and response text for debugging
         print(f"Response Content-Type: {response.headers['Content-Type']}")
-        print(f"Response Text: {response.text}")
+        pretty_json = json.dumps(response.json(), indent=4)
+        data = json.loads(pretty_json)
+        location_data = data["pageProps"]["location"]
+        filtered_data = {
+            "latitude": location_data["latitude"],
+            "longitude": location_data["longitude"],
+            "storeCode": location_data["storeCode"],
+            "businessHours": location_data["businessHours"]
+            }
+
         
-        # Try to parse the response as JSON
         if response.headers['Content-Type'] == 'application/json':
-            return response.json()
+            res = (json.dumps(filtered_data, indent=4))
+            print(res)
+            return res 
+
         else:
             print("The response is not in JSON format.")
             return None
@@ -26,7 +37,8 @@ def fetch_api_data(api_url):
 
 def main():
     api_url = "https://locations.wafflehouse.com/api/587d236eeb89fb17504336db/location-details"
-    data = fetch_api_data(api_url)
+    url3 = "https://locations.wafflehouse.com/_next/data/uEgKOOm2HjTXZT4HQfFmK/cartersville-ga-1718.json?slug=cartersville-ga-1718"
+    data = fetch_api_data(url3)
 
 
 if __name__ == "__main__":
